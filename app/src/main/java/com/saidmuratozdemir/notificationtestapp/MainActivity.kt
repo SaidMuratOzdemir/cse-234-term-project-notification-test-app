5f70f85ea7d2eb4b6dbaf79ac167bf571bb31640
Mehmet Akif ERGANİ <20210808045@ogr.akdeniz.edu.tr>
2024-05-20 23:05:44 +0300

    Imp: View of MainActivity improved

--- app/src/main/java/com/saidmuratozdemir/notificationtestapp/MainActivity.kt
+++ app/src/main/java/com/saidmuratozdemir/notificationtestapp/MainActivity.kt
@@ -6,12 +6,27 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.google.firebase.auth.FirebaseAuth
 
@@ -26,11 +41,51 @@ class MainActivity : ComponentActivity() {
             return
         } else {
             setContent {
-                Column(
+                MainScreen()
+
+            }
+        }
+    }
+
+    @Composable
+    @Preview
+    fun MainScreen() {
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.backgroundblur),
+                contentDescription = "Arka Plan Resmi",
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop
+            )
+
+            Box(modifier =Modifier.size(300.dp,580.dp)){
+
+                Image(
+                    painter = painterResource(id = R.drawable.background),
+                    contentDescription = "Arka Plan Resmi",
                     modifier = Modifier.fillMaxSize(),
-                    Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
+                    contentScale = ContentScale.Crop
+                )
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .shadow(
+                            elevation = 1.dp,
+                            shape = MaterialTheme.shapes.extraSmall,
+                        )
+                        .background(Color.Transparent),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+
+
+                    ) {
                     Image(
                         painter = painterResource(id = R.drawable.logo),
                         contentDescription = "App logo",
@@ -47,16 +102,19 @@ class MainActivity : ComponentActivity() {
                                 startActivity(Intent(this@MainActivity, LoginActivity::class.java))
                             },
                             modifier = Modifier.size(120.dp, 60.dp)
-                        ) { Text(text = "Log In") }
+                        ) { Text(text = "Sign In") }
                         Button(onClick = {
                             startActivity(Intent(this@MainActivity, RegisterActivity::class.java))
                         }, modifier = Modifier.size(120.dp, 60.dp)) {
-                            Text(text = "Register")
+                            Text(text = "Sign Up")
                         }
                     }
                 }
+
             }
+
         }
+
     }
 
     override fun onStart() {
