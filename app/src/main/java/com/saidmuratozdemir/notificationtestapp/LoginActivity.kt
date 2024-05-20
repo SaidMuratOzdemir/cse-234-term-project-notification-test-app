06a1ac1e360ef71cd5b105e28f952e7eaf167929
Mehmet Akif ERGANİ <20210808045@ogr.akdeniz.edu.tr>
2024-05-20 23:04:44 +0300

    Imp: view of login activity has improved

--- app/src/main/java/com/saidmuratozdemir/notificationtestapp/LoginActivity.kt
+++ app/src/main/java/com/saidmuratozdemir/notificationtestapp/LoginActivity.kt
@@ -5,16 +5,20 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -22,6 +26,10 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
@@ -44,47 +52,97 @@ class LoginActivity : ComponentActivity() {
                 var error by remember { mutableStateOf(false) }
                 val scope = rememberCoroutineScope()
 
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(16.dp),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
+                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Image(
+                        painter = painterResource(id = R.drawable.backgroundblur),
+                        modifier = Modifier.fillMaxSize(),
+                        contentDescription = "",
+                        contentScale = ContentScale.Crop
 
-                    OutlinedTextField(value = email, onValueChange = { email = it }, label = { Text("E-Mail") })
-                    Spacer(modifier = Modifier.height(8.dp))
-
-                    OutlinedTextField(
-                        value = password,
-                        onValueChange = { password = it },
-                        label = { Text("Password") },
-                        visualTransformation = PasswordVisualTransformation()
                     )
-                    Spacer(modifier = Modifier.height(16.dp))
+                    Box(modifier = Modifier.size(300.dp, 580.dp)) {
+                        Image(
+                            painter = painterResource(id = R.drawable.background),
+                            contentDescription = "Arka Plan Resmi",
+                            modifier = Modifier.fillMaxSize(),
+                            contentScale = ContentScale.Crop
+                        )
 
-                    Button(onClick = {
-                        scope.launch { // Launch coroutine for async work
-                            val result = signInWithEmailAndPassword(email, password)
-                            if (result.isFailure) {
-                                error = true
-                                val errorMessage = when (val exception = result.exceptionOrNull()) {
-                                    is FirebaseAuthInvalidUserException -> "Bu e-posta adresiyle kayıtlı bir kullanıcı yok."
-                                    is FirebaseAuthInvalidCredentialsException -> "E-posta veya şifre yanlış."
-                                    else -> "Giriş başarısız oldu. Lütfen tekrar deneyin."  // Genel hata mesajı
+
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(16.dp),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.logo),
+                                contentDescription = "App logo",
+                                modifier = Modifier.size(190.dp)
+                            )
+                            Spacer(modifier = Modifier.height(150.dp))
+
+                            OutlinedTextField(
+                                value = email,
+                                onValueChange = { email = it },
+                                label = { Text("E-Mail") },
+                                modifier = Modifier.shadow(
+                                    elevation = 1.dp,
+                                    shape = RoundedCornerShape(8.dp)
+                                ),
+                                colors = TextFieldDefaults.colors(focusedContainerColor = Color.Transparent, cursorColor = Color.Black)
+
+                            )
+                            Spacer(modifier = Modifier.height(8.dp))
+
+                            OutlinedTextField(
+                                value = password,
+                                onValueChange = { password = it },
+                                label = { Text("Password") },
+                                visualTransformation = PasswordVisualTransformation(),
+                                modifier = Modifier.shadow(
+                                    elevation = 1.dp,
+                                    shape = RoundedCornerShape(8.dp)
+                                ),
+                                colors = TextFieldDefaults.colors(focusedContainerColor = Color.Transparent, cursorColor = Color.Black)
+                            )
+                            Spacer(modifier = Modifier.height(16.dp))
+
+                            Button(onClick = {
+                                scope.launch { // Launch coroutine for async work
+                                    val result = signInWithEmailAndPassword(email, password)
+                                    if (result.isFailure) {
+                                        error = true
+                                        val errorMessage =
+                                            when (val exception = result.exceptionOrNull()) {
+                                                is FirebaseAuthInvalidUserException -> "Bu e-posta adresiyle kayıtlı bir kullanıcı yok."
+                                                is FirebaseAuthInvalidCredentialsException -> "E-posta veya şifre yanlış."
+                                                else -> "Giriş başarısız oldu. Lütfen tekrar deneyin."  // Genel hata mesajı
+                                            }
+                                        Toast.makeText(
+                                            this@LoginActivity, errorMessage, Toast.LENGTH_LONG
+                                        ).show()
+                                    } else {
+                                        Toast.makeText(
+                                            this@LoginActivity, "Başarılı", Toast.LENGTH_LONG
+                                        ).show()
+                                        finish()
+                                        return@launch
+                                    }
                                 }
-                                Toast.makeText(this@LoginActivity, errorMessage, Toast.LENGTH_LONG).show()
-                            } else {
-                                Toast.makeText(this@LoginActivity, "Başarılı", Toast.LENGTH_LONG).show()
-                                finish()
-                                return@launch
+
+                            }) {
+                                Text("Giriş Yap")
                             }
                         }
 
-                    }) {
-                        Text("Giriş Yap")
+
                     }
+
                 }
+
+
             }
         }
     }
