cc5f89c4936d8ae7552c67c570702b97972dd689
SaidMuratOzdemir <114745121+SaidMuratOzdemir@users.noreply.github.com>
2024-05-21 20:12:51 +0300

    feat: save button function added.

--- app/src/main/java/com/saidmuratozdemir/notificationtestapp/FirebaseConfigActivity.kt
+++ app/src/main/java/com/saidmuratozdemir/notificationtestapp/FirebaseConfigActivity.kt
@@ -1,6 +1,7 @@
 package com.saidmuratozdemir.notificationtestapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -30,17 +31,22 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
+import com.google.gson.Gson
 import com.saidmuratozdemir.notificationtestapp.components.Toolbar
+import com.saidmuratozdemir.notificationtestapp.dataClasses.FirebaseConfigurationObject
 import com.saidmuratozdemir.notificationtestapp.ui.theme.NotificationTestAppTheme
 
 class FirebaseConfigActivity : ComponentActivity() {
+    private lateinit var projectUrl: String
+    private lateinit var projectId: String
+    private lateinit var emailAddress: String
+    private lateinit var apiKey: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -63,27 +69,28 @@ class FirebaseConfigActivity : ComponentActivity() {
                         modifier = Modifier
                             .padding(start = 330.dp)
                             .padding(top = 3.dp)
-                            .clickable { }
+                            .clickable { saveButton(projectUrl, projectId, emailAddress, apiKey) }
                             .windowInsetsPadding(insets = WindowInsets.statusBars))
                 }
                 Column(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(top = 210.dp), Arrangement.Center,
+                        .padding(top = 210.dp),
+                    Arrangement.Center,
 
-                ) {
+                    ) {
 
-                    StringTextField("Project Url")
-                    StringTextField(text = "Project ID")
-                    StringTextField(text = "E-Mail Address")
-                    StringTextField(text = "Api Key")
+                    projectUrl = StringTextField("Project URL")
+                    projectId = StringTextField("Project ID")
+                    emailAddress = StringTextField("Email Address")
+                    apiKey = StringTextField("API Key")
                 }
             }
         }
     }
 
     @Composable
-    fun StringTextField(text: String) {
+    fun StringTextField(text: String): String {
         var textFieldValue by remember { mutableStateOf(TextFieldValue("")) }
 
         Row(
@@ -102,8 +109,7 @@ class FirebaseConfigActivity : ComponentActivity() {
                 fontWeight = FontWeight.SemiBold
             )
 
-            TextField(
-                value = textFieldValue,
+            TextField(value = textFieldValue,
                 onValueChange = { textFieldValue = it },
                 label = { Text("Enter $text") },
                 modifier = Modifier
@@ -113,5 +119,29 @@ class FirebaseConfigActivity : ComponentActivity() {
             )
         }
         Spacer(modifier = Modifier.height(15.dp))
+
+        return textFieldValue.text
+    }
+
+    private fun saveButton(
+        projectUrl: String, projectId: String, emailAddress: String, apiKey: String
+    ) {
+
+        if (projectUrl.isEmpty() || projectId.isEmpty() || emailAddress.isEmpty() || apiKey.isEmpty()) {
+            Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val firebaseConfiguration =
+            FirebaseConfigurationObject(projectUrl, projectId, emailAddress, apiKey)
+
+        val sharedPreferences = getSharedPreferences("notificationApp", MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        val jsonString = Gson().toJson(firebaseConfiguration)
+        editor.putString("firebaseConfiguration", jsonString)
+        editor.apply()
+
+        Toast.makeText(this, "Firebase configuration saved successfully!", Toast.LENGTH_SHORT)
+            .show()
     }
 }
