a94f0658c88c27ec3c159dfaa7f72717dae936ad
SaidMuratOzdemir <114745121+SaidMuratOzdemir@users.noreply.github.com>
2024-05-21 19:04:08 +0300

    imp: instead of FCM for notifications, we use local notifications from now on.

--- app/src/main/java/com/saidmuratozdemir/notificationtestapp/PermissionCheck.kt
+++ app/src/main/java/com/saidmuratozdemir/notificationtestapp/PermissionCheck.kt
@@ -1,25 +1,22 @@
 package com.saidmuratozdemir.notificationtestapp
 
 import android.app.AlertDialog
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.provider.Settings
-import com.google.firebase.messaging.Constants.MessagePayloadKeys.SENDER_ID
-import com.google.firebase.messaging.FirebaseMessaging
-import com.google.firebase.messaging.RemoteMessage
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
-import java.util.UUID
 import com.saidmuratozdemir.notificationtestapp.dataClasses.NotificationObject
 
 class PermissionCheck(private val context: Context) {
-
     fun checkNotificationPermission() {
-        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         if (!notificationManager.areNotificationsEnabled()) {
             AlertDialog.Builder(context)
@@ -35,35 +32,46 @@ class PermissionCheck(private val context: Context) {
                 .setNegativeButton("No", null)
                 .show()
         } else {
-            FirebaseMessaging.getInstance().subscribeToTopic("/topics/defaultTopic")
-                .addOnCompleteListener { task ->
-                    if (task.isSuccessful) {
-                        val messageId = UUID.randomUUID().toString()
-                        val message = RemoteMessage.Builder("${SENDER_ID}@fcm.googleapis.com")
-                            .setMessageId(messageId)
-                            .addData("my_message", "Hello World")
-                            .addData("my_action","SAY_HELLO")
-                            .build()
-                        FirebaseMessaging.getInstance().send(message)
-                        val sharedPref = context.getSharedPreferences("sharedPref", Context.MODE_PRIVATE)
-                        val editor = sharedPref.edit()
-                        val gson = Gson()
-                        val json = sharedPref.getString("NotificationHistory", null)
-                        val type = object : TypeToken<ArrayList<NotificationObject>>() {}.type
-                        val notificationList: ArrayList<NotificationObject> = gson.fromJson(json, type) ?: arrayListOf()
-                        val currentDateTime = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                            LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
-                        } else {
-                            "N/A"
-                        }
-                        notificationList.add(NotificationObject("New Message", "Hello World", currentDateTime))
-                        val newJson = gson.toJson(notificationList)
-                        editor.putString("NotificationHistory", newJson)
-                        editor.apply()
-                    } else {
-                        // Subscription failed
-                    }
+            val sharedPref = context.getSharedPreferences("notificationApp", Context.MODE_PRIVATE)
+            sharedPref.edit()
+                .putInt("lastNotificationId", sharedPref.getInt("lastNotificationId", 0) + 1)
+                .apply()
+            val lastNotificationId = sharedPref.getInt("lastNotificationId", 0)
+            val channelId = "notification_test_app_44"
+            val channelName = "Notification Channel"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                notificationManager.createNotificationChannel(
+                    NotificationChannel(
+                        channelId,
+                        channelName,
+                        importance
+                    )
+                )
+            }
+
+            val myNotificationObject = NotificationObject(
+                "New Message Title",
+                "New message text. Notification ID: $lastNotificationId",
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+                } else {
+                    "N/A"
                 }
+            )
+
+            val gson = Gson()
+            val json = sharedPref.getString("NotificationHistory", null)
+            val type = object : TypeToken<ArrayList<NotificationObject>>() {}.type
+            val notificationList: ArrayList<NotificationObject> =
+                gson.fromJson(json, type) ?: arrayListOf()
+            notificationList.add(
+                myNotificationObject
+            )
+            val newJson = gson.toJson(notificationList)
+            sharedPref.edit()
+                .putString("NotificationHistory", newJson)
+                .apply()
         }
     }
 }
