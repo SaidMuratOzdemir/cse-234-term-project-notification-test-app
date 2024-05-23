3c5916f4c29391d63191ae9d3d73a288dfdd811a
Mehmet Akif ERGANİ <20210808045@ogr.akdeniz.edu.tr>
2024-05-23 15:56:32 +0300

    imp: save button function added FirebaseConfigActivity

--- app/src/main/java/com/saidmuratozdemir/notificationtestapp/FirebaseConfigActivity.kt
+++ app/src/main/java/com/saidmuratozdemir/notificationtestapp/FirebaseConfigActivity.kt
@@ -1,5 +1,7 @@
-package com.saidmuratozdemir.notificationtestapp
+package com.saidmuratozdemir.notificationtestapp // Paket adınızı buraya ekleyin
 
+// Gerekli importlar
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -7,19 +9,16 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.statusBars
-import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -35,6 +34,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import com.google.gson.Gson
@@ -43,52 +43,78 @@ import com.saidmuratozdemir.notificationtestapp.dataClasses.FirebaseConfiguratio
 import com.saidmuratozdemir.notificationtestapp.ui.theme.NotificationTestAppTheme
 
 class FirebaseConfigActivity : ComponentActivity() {
-    private lateinit var projectUrl: String
-    private lateinit var projectId: String
-    private lateinit var emailAddress: String
-    private lateinit var apiKey: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            NotificationTestAppTheme {
 
-                Box(modifier = Modifier.fillMaxSize()) {
-                    Image(
-                        painter = painterResource(id = R.drawable.backgroundblur),
-                        contentDescription = "back",
-                        modifier = Modifier.fillMaxSize(),
-                        contentScale = ContentScale.Crop
-                    )
-                }
-                Box(modifier = Modifier.fillMaxSize()) {
-                    Toolbar("FIREBASE CONFIGURATIONS", "Firebase Configuration Settings")
-                    Image(painter = painterResource(id = R.drawable.save),
-                        contentDescription = "save button",
-                        modifier = Modifier
-                            .padding(start = 330.dp)
-                            .padding(top = 3.dp)
-                            .clickable { saveButton(projectUrl, projectId, emailAddress, apiKey) }
-                            .windowInsetsPadding(insets = WindowInsets.statusBars))
-                }
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = 210.dp),
-                    Arrangement.Center,
-                ) {
-                    projectUrl = StringTextField("Project URL")
-                    projectId = StringTextField("Project ID")
-                    emailAddress = StringTextField("Email Address")
-                    apiKey = StringTextField("API Key")
-                }
-            }
+        setContent {
+            FirebaseConfigScreen()
         }
     }
 
     @Composable
-    fun StringTextField(text: String): String {
-        var textFieldValue by remember { mutableStateOf(TextFieldValue("")) }
+    fun FirebaseConfigScreen() {
+        val sharedPrefs = getSharedPreferences("notificationApp", MODE_PRIVATE)
+        var firebaseConfig by remember { mutableStateOf(loadFirebaseConfig(sharedPrefs)) }
+
+        NotificationTestAppTheme {
+            Box(modifier = Modifier.fillMaxSize()) {
+                Image(
+                    painter = painterResource(id = R.drawable.backgroundblur),
+                    contentDescription = "back",
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Crop
+                )
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(top = 110.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    StringTextField("Project URL", firebaseConfig.projectUrl) { projectUrl ->
+                        firebaseConfig = firebaseConfig.copy(projectUrl = projectUrl)
+                    }
+                    StringTextField("Project ID", firebaseConfig.projectId) { projectId ->
+                        firebaseConfig = firebaseConfig.copy(projectId = projectId)
+                    }
+                    StringTextField("Email Address", firebaseConfig.emailAddress) { emailAddress ->
+                        firebaseConfig = firebaseConfig.copy(emailAddress = emailAddress)
+                    }
+                    StringTextField(
+                        "API Key",
+                        firebaseConfig.apiKey,
+                        visualTransformation = PasswordVisualTransformation()
+                    ) { apiKey ->
+                        firebaseConfig = firebaseConfig.copy(apiKey = apiKey)
+                    }
+
+
+                }
+
+                Toolbar("FIREBASE CONFIGURATIONS", "Firebase Configuration Settings")
+                Image(painter = painterResource(id = R.drawable.save),
+                    contentDescription = "save button",
+                    modifier = Modifier
+                        .size(50.dp)
+                        .offset(340.dp, 32.dp)
+                        .clickable {
+                            saveButton(firebaseConfig, sharedPrefs)
+                        })
+            }
+        }
+    }
+
+
+    @Composable
+    fun StringTextField(
+        text: String,
+        initialValue: String,
+        visualTransformation: androidx.compose.ui.text.input.VisualTransformation = androidx.compose.ui.text.input.VisualTransformation.None,
+        onValueChange: (String) -> Unit // Lambda for updating the state
+    ) {
+        var textFieldValue by remember { mutableStateOf(TextFieldValue(initialValue)) }
 
         Row(
             modifier = Modifier
@@ -100,45 +126,47 @@ class FirebaseConfigActivity : ComponentActivity() {
                 modifier = Modifier
                     .padding(bottom = 4.dp)
                     .align(Alignment.CenterVertically)
-                    .size(110.dp, 20.dp),
+                    .weight(0.3f), // Adjust the weight for better layout
                 maxLines = 1,
                 color = Color.Black,
                 fontWeight = FontWeight.SemiBold
             )
 
             TextField(value = textFieldValue,
-                onValueChange = { textFieldValue = it },
+                onValueChange = {
+                    textFieldValue = it
+                    onValueChange(it.text) // Update the state
+                },
                 label = { Text("Enter $text") },
                 modifier = Modifier
                     .fillMaxWidth()
-                    .size(50.dp)
-                    .clip(CircleShape)
+                    .weight(0.7f) // Adjust the weight for better layout
+                    .clip(CircleShape),
+                visualTransformation = visualTransformation
             )
         }
         Spacer(modifier = Modifier.height(15.dp))
-
-        return textFieldValue.text
     }
 
-    private fun saveButton(
-        projectUrl: String, projectId: String, emailAddress: String, apiKey: String
-    ) {
 
-        if (projectUrl.isEmpty() || projectId.isEmpty() || emailAddress.isEmpty() || apiKey.isEmpty()) {
+    private fun saveButton(
+        firebaseConfig: FirebaseConfigurationObject, sharedPrefs: SharedPreferences
+    ) {
+        if (firebaseConfig.projectUrl.isEmpty() || firebaseConfig.projectId.isEmpty() || firebaseConfig.emailAddress.isEmpty() || firebaseConfig.apiKey.isEmpty()) {
             Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show()
             return
         }
-
-        val firebaseConfiguration =
-            FirebaseConfigurationObject(projectUrl, projectId, emailAddress, apiKey)
-
-        val sharedPreferences = getSharedPreferences("notificationApp", MODE_PRIVATE)
-        val editor = sharedPreferences.edit()
-        val jsonString = Gson().toJson(firebaseConfiguration)
+        val editor = sharedPrefs.edit()
+        val jsonString = Gson().toJson(firebaseConfig)
         editor.putString("firebaseConfiguration", jsonString)
         editor.apply()
-
         Toast.makeText(this, "Firebase configuration saved successfully!", Toast.LENGTH_SHORT)
             .show()
+    }
+
+    private fun loadFirebaseConfig(sharedPrefs: SharedPreferences): FirebaseConfigurationObject {
+        val jsonString = sharedPrefs.getString("firebaseConfiguration", null)
+        return Gson().fromJson(jsonString, FirebaseConfigurationObject::class.java)
+            ?: FirebaseConfigurationObject("", "", "", "")
     }
 }
